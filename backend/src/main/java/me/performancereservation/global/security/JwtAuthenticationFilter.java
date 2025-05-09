40330f95631140d1fc67e650cca8c747e741a845
silkair <silkair1234@gmail.com>
2025-05-10 03:56:15 +0900

    feat(security) : JwtAuthenticationFilter 구현

---
+++ backend/src/main/java/me/performancereservation/global/security/JwtAuthenticationFilter.java
@@ -0,0 +1,35 @@
+package me.performancereservation.global.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+        String accessToken = jwtTokenProvider.resolveToken(request); //Authorization 헤더에서 Bearer 토큰 추출
+        //토큰이 유효하면 SecurityContext에 인증 정보 저장
+        if(accessToken !=null && jwtTokenProvider.validateToken(accessToken)) {
+                Authentication auth = jwtTokenProvider.getAuthentication(accessToken);
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        //다음 필터로 요청 전달
+        filterChain.doFilter(request, response);
+
+    }
+}
