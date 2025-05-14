2b4ded90499e6a2245ac3be227bfe3d33f1c7d23
inswal843 <inswal843@gmail.com>
2025-05-14 10:26:18 +0900

    fix(admin-approve): 이전 쿼리 주석 삭제

--- backend/src/main/java/me/performancereservation/domain/admin/repository/AdminManagerRequestRepository.java
+++ backend/src/main/java/me/performancereservation/domain/admin/repository/AdminManagerRequestRepository.java
@@ -9,20 +9,5 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface AdminManagerRequestRepository extends JpaRepository<ManagerRequest, Long> {
-    /** 관리자가 상태를 이용해 공연 관리자 신청 목록을 조회하는 쿼리
-     *
-     * ManagerRequest 테이블에서 받은 상태에 해당하는 공연 관리자 신청 목록을 조회
-     * 최신순 출력을 위해 생성 시간 내림차순 정렬
-     * @param pageable
-     * @return Page<ManagerRequest>
-     */
-//    @Query("""
-//        SELECT m
-//        FROM ManagerRequest m
-//        WHERE m.status = :status
-//        ORDER BY m.createdAt DESC
-//    """)
-//    Page<ManagerRequest> findPendingManagerRequests(@Param("status") ManagerRequestStatus status, Pageable pageable);
-
     Page<ManagerRequest> findAllByStatusOrderByCreatedAt(ManagerRequestStatus status, Pageable pageable);
 }
--- backend/src/main/java/me/performancereservation/domain/admin/repository/AdminPerformanceRepository.java
+++ backend/src/main/java/me/performancereservation/domain/admin/repository/AdminPerformanceRepository.java
@@ -10,20 +10,5 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface AdminPerformanceRepository extends JpaRepository<Performance, Long> {
-    /** 관리자가 상태를 이용해 공연 목록을 조회하는 쿼리
-     *
-     * Performance 테이블에서 받은 상태에 해당하는 공연 목록을 조회
-     * 최신순 출력을 위해 생성 시간 내림차순 정렬
-     * @param pageable
-     * @return Page<Performance>
-     */
-//    @Query("""
-//        SELECT p
-//        FROM Performance p
-//        WHERE p.status = :status
-//        ORDER BY p.createdAt DESC
-//    """)
-//    Page<Performance> findPendingPerformances(@Param("status") PerformanceStatus status, Pageable pageable);
-
     Page<Performance> findAllByStatusOrderByCreatedAt(PerformanceStatus status, Pageable pageable);
 }
