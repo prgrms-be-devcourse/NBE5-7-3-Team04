3fe02a6b2167da33f7262d651e16a388cd797d02
황치연 <119822911+inswal843@users.noreply.github.com>
2025-05-14 10:59:55 +0900

    fix(admin-approve): ErrorCode 세미콜론 수정

--- backend/src/main/java/me/performancereservation/global/exception/ErrorCode.java
+++ backend/src/main/java/me/performancereservation/global/exception/ErrorCode.java
@@ -51,7 +51,7 @@ public enum ErrorCode {
     //공연자 승인 관련
     MANAGER_REQUEST_NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 공연 관리자 요청을 찾을 수 없습니다."),
     MANAGER_REQUEST_STATUS_NOT_PENDING(HttpStatus.BAD_REQUEST, "PENDING 상태의 공연 관리자 요청만 승인, 거부 할 수 있습니다."),
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 사용자를 찾을 수 없습니다.");
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 사용자를 찾을 수 없습니다."),
 
     // 예약 관련
     RESERVATION_NOT_FOUND(HttpStatus.NOT_FOUND,  "해당하는 예약이 없습니다."),
