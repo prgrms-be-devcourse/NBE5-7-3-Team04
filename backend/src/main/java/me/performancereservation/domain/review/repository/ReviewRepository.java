80e410748f6f44275e4b55e908460aeaf7fd96b6
silkair <silkair1234@gmail.com>
2025-05-14 17:05:59 +0900

    feat(review) : review 리스트 페이징 처리

--- backend/src/main/java/me/performancereservation/domain/review/repository/ReviewRepository.java
+++ backend/src/main/java/me/performancereservation/domain/review/repository/ReviewRepository.java
@@ -1,16 +1,14 @@
 package me.performancereservation.domain.review.repository;
 
 import me.performancereservation.domain.review.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-    @Query("""
-        SELECT r FROM Review r
-        WHERE r.performanceId = :performanceId
-        ORDER BY r.createdAt DESC
-    """)
-    List<Review> findByPerformanceIdOrderByCreatedAtDesc(Long performanceId);
+    //리뷰 페이징
+    Page<Review> findByPerformanceIdOrderByCreatedAtDesc(Long performanceId, Pageable pageable);
 }
--- backend/src/main/java/me/performancereservation/domain/review/service/ReviewService.java
+++ backend/src/main/java/me/performancereservation/domain/review/service/ReviewService.java
@@ -8,9 +8,11 @@ import me.performancereservation.domain.review.mapper.ReviewMapper;
 import me.performancereservation.domain.review.repository.ReviewRepository;
 import me.performancereservation.domain.user.entitiy.User;
 import me.performancereservation.domain.user.repository.UserRepository;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.awt.print.Pageable;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -37,15 +39,14 @@ public class ReviewService {
 
     //리뷰 리스트 조회
     @Transactional
-    public List<ReviewResponse> getReviewsByPerformanceId(Long performanceId) {
-        List<Review> reviews = reviewRepository.findByPerformanceIdOrderByCreatedAtDesc(performanceId);
+    public Page<ReviewResponse> getReviewsByPerformanceId(Long performanceId, Pageable pageable) {
+        Page<Review> reviews = reviewRepository.findByPerformanceIdOrderByCreatedAtDesc(performanceId,pageable);
         //유저 id 모아서 한번에 유조 조회하고 맵핑!
         List<Long> userIds = reviews.stream().map(Review::getUserId).distinct().toList();
         List<User> users = userRepository.findAllById(userIds);
 
         Map<Long, User> userMap = users.stream().collect(Collectors.toMap(User::getId, user -> user));
 
-        return reviews.stream()
-                .map(review -> reviewMapper.toReviewResponse(review, userMap.get(review.getUserId()))).toList();
+        return reviews.map(review -> reviewMapper.toReviewResponse(review, userMap.get(review.getUserId())));
     }
 }
