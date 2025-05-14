b7042665e0974f5545312d1e35ed9d0db60964dd
황치연 <119822911+inswal843@users.noreply.github.com>
2025-05-14 11:10:35 +0900

    fix(admin-approve): 겹치는 ErrorCode 통일

--- backend/src/main/java/me/performancereservation/global/exception/ErrorCode.java
+++ backend/src/main/java/me/performancereservation/global/exception/ErrorCode.java
@@ -45,13 +45,11 @@ public enum ErrorCode {
     SCHEDULE_ALREADY_CANCELED(HttpStatus.CONFLICT, "이미 취소된 회차입니다."),
 
     // 공연 승인 관련
-    PERFORMANCE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 공연을 찾을 수 없습니다."),
     PERFORMANCE_STATUS_NOT_PENDING(HttpStatus.BAD_REQUEST, "PENDING 상태의 공연만 승인, 거부 할 수 있습니다."),
 
     //공연자 승인 관련
     MANAGER_REQUEST_NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 공연 관리자 요청을 찾을 수 없습니다."),
     MANAGER_REQUEST_STATUS_NOT_PENDING(HttpStatus.BAD_REQUEST, "PENDING 상태의 공연 관리자 요청만 승인, 거부 할 수 있습니다."),
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 사용자를 찾을 수 없습니다."),
 
     // 예약 관련
     RESERVATION_NOT_FOUND(HttpStatus.NOT_FOUND,  "해당하는 예약이 없습니다."),
