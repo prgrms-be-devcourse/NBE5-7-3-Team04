959d6b8323c57197b1e4863199825c92f97b9de1
이상진 <silkair1234@gmail.com>
2025-05-14 11:27:52 +0900

    fix : 불필요한 주석 제거

--- backend/src/main/java/me/performancereservation/domain/user/dto/UserOnboardingRequest.java
+++ backend/src/main/java/me/performancereservation/domain/user/dto/UserOnboardingRequest.java
@@ -2,5 +2,5 @@ package me.performancereservation.domain.user.dto;
 
 public record UserOnboardingRequest (
         String phoneNumber,
-        String email //카카오 로그인인 경우에만 입력 받기.
+        String email
 ){}
