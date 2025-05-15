98b89df36b4ed9f03cab6a9b5d532a4a419ad0cf
inswal843 <inswal843@gmail.com>
2025-05-15 09:47:49 +0900

    chore(bookmark): 응답 항목 주석 추가

--- backend/src/main/java/me/performancereservation/domain/bookmark/dto/BookmarkedPerformancePageResponse.java
+++ backend/src/main/java/me/performancereservation/domain/bookmark/dto/BookmarkedPerformancePageResponse.java
@@ -11,24 +11,23 @@ import java.time.LocalDateTime;
  * @param fileUrl
  * @param title
  * @param price
- * @param performanceDate
+ * @param startDate
+ * @param endDate
  * @param venue
- * @param description
  * @param category
  * @param status
  * @param bookmarked //북마크 되어있는지 여부
  */
 public record BookmarkedPerformancePageResponse(
-        long id,
-        String fileUrl,
-        String title,
-        int price,
-        LocalDateTime startDate,
-        LocalDateTime endDate,
-        String venue,
-        String description,
-        PerformanceCategory category,
-        PerformanceStatus status,
-        boolean bookmarked
+        long id,                            // 공연 ID
+        String fileUrl,                     // 공연 이미지 URL
+        String title,                       // 공연 제목
+        int price,                          // 공연 가격
+        LocalDateTime startDate,            // 공연 시작일
+        LocalDateTime endDate,              // 공연 종료일
+        String venue,                       // 공연 장소
+        PerformanceCategory category,       // 공연 카테고리
+        PerformanceStatus status,           // 공연 상태
+        boolean bookmarked                  // 공연 북마크 여부
 ) {
 }
