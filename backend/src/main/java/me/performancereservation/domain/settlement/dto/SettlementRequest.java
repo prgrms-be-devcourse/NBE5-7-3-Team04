bc57079e074c4e740b56c3ce181313e1302e67f9
Seoyoon Tak <luckyoon0314@kaist.ac.kr>
2025-05-15 11:04:00 +0900

    chore(refund): request는 래퍼타입으로 변경

--- backend/src/main/java/me/performancereservation/domain/settlement/dto/SettlementRequest.java
+++ backend/src/main/java/me/performancereservation/domain/settlement/dto/SettlementRequest.java
@@ -3,7 +3,7 @@ package me.performancereservation.domain.settlement.dto;
 import jakarta.validation.constraints.NotNull;
 
 public record SettlementRequest(
-    @NotNull long performanceId,
+    @NotNull Long performanceId,
     @NotNull String account,
     @NotNull String bank
 ) {
