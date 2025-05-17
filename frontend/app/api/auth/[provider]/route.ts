a05bb1b5f05bcb8a3ed5f2ad3b3258a3f7f0184d
silkair <silkair1234@gmail.com>
2025-05-17 15:01:32 +0900

    feat(login) : 배포용 pr url 수정

--- frontend/app/api/auth/[provider]/route.ts
+++ frontend/app/api/auth/[provider]/route.ts
@@ -5,8 +5,8 @@ export async function GET(request: NextRequest, { params }: { params: { provider
   const provider = params.provider
 
   // 실제 구현에서는 백엔드 API로 리다이렉트
-  // const redirectUrl = `http://43.201.79.165:8080/oauth2/authorization/${provider}`
-  const redirectUrl = `http://localhost:8080/oauth2/authorization/${provider}`
+   const redirectUrl = `http://43.201.79.165:8080/oauth2/authorization/${provider}`
+//  const redirectUrl = `http://localhost:8080/oauth2/authorization/${provider}`
 
   // 리다이렉트
   return NextResponse.redirect(redirectUrl)
