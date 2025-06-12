1479d24da4797dbda5468750a76cfd506a5f62dd
kwang2134 <kwang2134@naver.com>
2025-06-12 17:20:21 +0900

    refactor(admin-reservation): 어드민 예약 service 마이그레이션

--- backend/src/main/kotlin/me/performancereservation/domain/admin/service/AdminReservationService.kt
+++ backend/src/main/kotlin/me/performancereservation/domain/admin/service/AdminReservationService.kt
@@ -1,32 +1,29 @@
-package me.performancereservation.domain.admin.service;
+package me.performancereservation.domain.admin.service
 
-import lombok.RequiredArgsConstructor;
-import me.performancereservation.domain.admin.dto.AdminReservationPageResponse;
-import me.performancereservation.domain.admin.repository.AdminReservationRepository;
-import me.performancereservation.domain.performance.entities.PerformanceSchedule;
-import me.performancereservation.domain.performance.repository.PerformanceScheduleRepository;
-import me.performancereservation.domain.reservation.Reservation;
-import me.performancereservation.domain.reservation.ReservationRepository;
-import me.performancereservation.domain.reservation.enums.ReservationStatus;
-import me.performancereservation.domain.sms.SMSService;
-import me.performancereservation.domain.user.entitiy.User;
-import me.performancereservation.domain.user.repository.UserRepository;
-import me.performancereservation.global.exception.ErrorCode;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDateTime;
+import lombok.RequiredArgsConstructor
+import me.performancereservation.domain.admin.dto.AdminReservationPageResponse
+import me.performancereservation.domain.admin.repository.AdminReservationRepository
+import me.performancereservation.domain.performance.repository.PerformanceScheduleRepository
+import me.performancereservation.domain.reservation.ReservationRepository
+import me.performancereservation.domain.reservation.enums.ReservationStatus
+import me.performancereservation.domain.sms.SMSService
+import me.performancereservation.domain.user.repository.UserRepository
+import me.performancereservation.global.exception.ErrorCode
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.time.LocalDateTime
 
 @Service
 @RequiredArgsConstructor
-public class AdminReservationService {
-    private final SMSService smsService;
-    private final UserRepository userRepository;
-    private final ReservationRepository reservationRepository;
-    private final AdminReservationRepository adminReservationRepository;
-    private final PerformanceScheduleRepository performanceScheduleRepository;
+class AdminReservationService (
+    private val smsService: SMSService,
+    private val userRepository: UserRepository,
+    private val reservationRepository: ReservationRepository,
+    private val adminReservationRepository: AdminReservationRepository,
+    private val performanceScheduleRepository: PerformanceScheduleRepository,
+){
 
     /** 관리자 예약 목록 조회
      *
@@ -34,8 +31,8 @@ public class AdminReservationService {
      * @return AdminReservationPageResponse
      */
     @Transactional(readOnly = true)
-    public Page<AdminReservationPageResponse> getReservationList(Pageable pageable) {
-        return adminReservationRepository.findAdminReservations(pageable);
+    fun getReservationList(pageable: Pageable): Page<AdminReservationPageResponse> {
+        return adminReservationRepository!!.findAdminReservations(pageable)
     }
 
     /** 관리자 예약 목록 검색
@@ -49,13 +46,22 @@ public class AdminReservationService {
      * @return AdminReservationPageResponse
      */
     @Transactional(readOnly = true)
-    public Page<AdminReservationPageResponse> searchReservationList(Pageable pageable,
-                                                                    String userName,
-                                                                    String performanceTitle,
-                                                                    ReservationStatus reservationStatus,
-                                                                    LocalDateTime startDate,
-                                                                    LocalDateTime endDate) {
-        return adminReservationRepository.searchAdminReservations(userName, performanceTitle, reservationStatus, startDate, endDate, pageable);
+    fun searchReservationList(
+        pageable: Pageable,
+        userName: String?,
+        performanceTitle: String?,
+        reservationStatus: ReservationStatus?,
+        startDate: LocalDateTime?,
+        endDate: LocalDateTime?
+    ): Page<AdminReservationPageResponse> {
+        return adminReservationRepository!!.searchAdminReservations(
+            userName,
+            performanceTitle,
+            reservationStatus,
+            startDate,
+            endDate,
+            pageable
+        )
     }
 
     /** 관리자 예약 확정 상태 변경
@@ -64,18 +70,25 @@ public class AdminReservationService {
      * @param reservationId 예약 id
      */
     @Transactional
-    public void confirmReservation(Long reservationId) {
-        Reservation reservation = reservationRepository.findById(reservationId)
-                .orElseThrow(() -> ErrorCode.RESERVATION_NOT_FOUND.domainException("예약이 존재하지 않습니다."));
+    fun confirmReservation(reservationId: Long) {
+        val reservation = reservationRepository!!.findById(reservationId)
+            .orElseThrow {
+                ErrorCode.RESERVATION_NOT_FOUND.domainException(
+                    "예약이 존재하지 않습니다."
+                )
+            }
 
-        PerformanceSchedule schedule = performanceScheduleRepository.findById(reservation.getScheduleId())
-                .orElseThrow(() -> ErrorCode.PERFORMANCE_SCHEDULE_NOT_FOUND.domainException("회차가 존재하지 않습니다."));
+        val schedule = performanceScheduleRepository!!.findById(reservation.scheduleId)
+            .orElseThrow {
+                ErrorCode.PERFORMANCE_SCHEDULE_NOT_FOUND.domainException(
+                    "회차가 존재하지 않습니다."
+                )
+            }
 
-        reservation.confirm();
-        schedule.decreaseRemainingSeats(reservation.getQuantity());
+        reservation.confirm()
+        schedule.decreaseRemainingSeats(reservation.quantity)
 
         // 예약 승인 안내 문자
 //        smsService.reservationConfirmed(reservation);
     }
-
 }
