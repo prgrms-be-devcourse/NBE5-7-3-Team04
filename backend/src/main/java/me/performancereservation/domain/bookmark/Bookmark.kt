dfac4f9eb9ab6794e68b71590faad9574f9fbf1a
inswal843 <inswal843@gmail.com>
2025-06-12 12:47:37 +0900

    feat(bookmark): bookmark equals(), hashCode() 재정의

--- backend/src/main/java/me/performancereservation/domain/bookmark/Bookmark.kt
+++ backend/src/main/java/me/performancereservation/domain/bookmark/Bookmark.kt
@@ -12,4 +12,24 @@ class Bookmark(
 
     val userId: Long,           // (FK)사용자 ID
     val performanceId: Long     // (FK)공연 ID
-) : BaseEntity()
+) : BaseEntity() {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Bookmark
+
+        if (id != other.id) return false
+        if (userId != other.userId) return false
+        if (performanceId != other.performanceId) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id?.hashCode() ?: 0
+        result = 31 * result + userId.hashCode()
+        result = 31 * result + performanceId.hashCode()
+        return result
+    }
+}
