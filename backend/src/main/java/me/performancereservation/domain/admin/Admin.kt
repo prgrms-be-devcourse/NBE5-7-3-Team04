b5157ad194a93c95ed4bccf0d539a91bd25839e5
inswal843 <inswal843@gmail.com>
2025-06-12 15:44:18 +0900

    feat(admin): admin role 필드에 Enumerated 추가

--- backend/src/main/java/me/performancereservation/domain/admin/Admin.kt
+++ backend/src/main/java/me/performancereservation/domain/admin/Admin.kt
@@ -1,18 +1,19 @@
 package me.performancereservation.domain.admin
 
 import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
 import jakarta.persistence.Id
-import lombok.Getter
-import lombok.NoArgsConstructor
 import me.performancereservation.domain.user.enums.Role
 
 @Entity
-class Admin (
+class Admin(
     @Id
     val id: String,                 // 어드민 ID
 
     var password: String,           // 어드민 비밀번호
 
+    @Enumerated(EnumType.STRING)
     val role: Role = Role.ADMIN     // 어드민 권한 고정
 ) {
     fun changePassword(password: String) {
