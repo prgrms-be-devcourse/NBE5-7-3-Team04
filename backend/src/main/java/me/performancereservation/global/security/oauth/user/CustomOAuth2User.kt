e6f163d0436445e349a2c9d89b86b0c6886b54ab
silkair <silkair1234@gmail.com>
2025-06-13 09:51:12 +0900

    feat(security) : OAuth2SuccessHandler 마이그레이션

--- backend/src/main/java/me/performancereservation/global/security/oauth/user/CustomOAuth2User.kt
+++ backend/src/main/java/me/performancereservation/global/security/oauth/user/CustomOAuth2User.kt
@@ -9,7 +9,7 @@ import java.util.List
 //인증된 사용자 정보를 Spring Security의 Principal로 감싸서 반환
 //User 엔티티 전체를 노출하지 않고 필요한 정보만 노출
 class CustomOAuth2User (
-    private val user: User,
+    internal val user: User,
     private val attributes: Map<String, Any>
 ) : OAuth2User {
 
