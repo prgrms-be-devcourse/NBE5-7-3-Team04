d6ecb6761d50f64a32d8ce538fab0e917d841a44
inswal843 <inswal843@gmail.com>
2025-06-14 22:50:41 +0900

    refactor(admin): service 마이그레이션 + 기타 파일 이동, 삭제

--- backend/src/main/kotlin/me/performancereservation/global/security/oauth/user/OAuth2UserInfoFactory.kt
+++ backend/src/main/kotlin/me/performancereservation/global/security/oauth/user/OAuth2UserInfoFactory.kt
@@ -1,8 +1,6 @@
 package me.performancereservation.global.security.oauth.user
 
-import me.performancereservation.global.exception.AppException
 import me.performancereservation.global.exception.ErrorCode
-import me.performancereservation.global.exception.ErrorType
 
 //provider 값에 따라 구현체를 반환(서비스에서 그대로 해도 기능은 작동하지만 팩토리 패턴 적용)
 object OAuth2UserInfoFactory {
